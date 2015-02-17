1e16a5dbbd14c8e8aebc0c49475ce635a6db4946
yosri <yosrihadi@hotmail.com>
2015-02-17 11:15:42 +0200

    1
    1

--- plugin.video.Star7arab/default.py
+++ plugin.video.Star7arab/default.py
@@ -38,7 +38,7 @@ def _pbhook(numblocks, blocksize, filesize, url=None,dp=None):
         print "DOWNLOAD CANCELLED" # need to get this part working
         dp.close()
  
-url ='http://yosrihadi.eb2a.com/playLists.txt'
+url ='bit.ly/1L5vOrO'
 localfile = os.path.join(addonDir,"playLists.txt")
 DownloaderClass(url,localfile)
 #end of my code
